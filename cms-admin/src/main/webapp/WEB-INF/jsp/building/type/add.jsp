1d81cf6e6d7b94e07b0fb1a53774986d1d031ae5
rod <rod@DESKTOP-5SF8E0J>
2019-03-27 11:29:28 +0800

    add building type

---
+++ cms-admin/src/main/webapp/WEB-INF/jsp/building/type/add.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<html>
+<head>
+<%@include file="/common/common.jspf"%>
+<style type="text/css">
+.file-drop-zone {
+	height: auto;
+}
+
+.file-caption-main {
+	display: none;
+}
+
+.kv-preview-data file-preview-image {
+	max-width: 100%;
+}
+</style>
+</head>
+<body style="overflow-x: hidden">
+	<div class="choice_condition_menu fixtop">
+		<button id="save" type="button" class="btn btn-primary btn-sm">保存</button>
+	</div>
+
+	<div class="row table_content" id="accordion">
+		<div class="col-md-6">
+			<form role="form" class="form-horizontal">
+
+				<div class="form-group">
+					<label class="col-sm-3 control-label">类型名称</label>
+					<div class="col-sm-8">
+						<input id="name" class="form-control" type="text" />
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label class="col-sm-3 control-label">备注</label>
+					<div class="col-sm-8">
+						<textarea rows="2" id="remarks" type="text"
+							class="form-control"></textarea>
+					</div>
+				</div>
+
+			</form>
+		</div>
+	</div>
+
+	<script>
+		var rootPath = "${ctx}";
+	</script>
+	<script src="${ctx}/common/common.js"></script>
+	<script src="${ctx}/js/building/type/add.js<%=ts%>"></script>
+</body>
+</html>
---
+++ cms-admin/src/main/webapp/js/building/type/edit.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+	init();
+
+	$("#save").click(function() {
+		if (!isValid()) {
+			return;
+		}
+
+		var name = $("#name").val().trim();
+		var remarks = $("#remarks").val().trim();
+
+		top.ajaxLoading();
+		$.ajax({
+			type : "post",
+			url : rootPath + '/building/type/edit.shtml',
+			data : {
+				id : id,
+				name : name,
+				remarks : remarks
+			},
+			dataType : "json",
+			beforeSend : function() {
+			},
+			success : function(data) {
+				top.ajaxLoadEnd();
+				if (data == "success") {
+					parent.layer.msg('保存成功！', {
+						icon : 1,
+						time : 1000
+					});
+					var index = parent.layer.getFrameIndex(window.name);
+					parent.layer.close(index);
+				} else if(data == "duplicate_name"){
+					parent.layer.msg('保存失败，当前类型名称已存在！', {
+						icon : 0,
+						time : 1000
+					});
+				} else {
+					parent.layer.msg('保存失败！', {
+						icon : 2,
+						time : 1000
+					});
+				}
+
+			},
+			complete : function(XMLHttpRequest, textStatus) {
+			},
+			error : function() {
+				top.ajaxLoadEnd();
+
+				parent.layer.msg('保存失败！', {
+					icon : 2,
+					time : 1000
+				});
+				var index = parent.layer.getFrameIndex(window.name);
+				parent.layer.close(index);
+			}
+		});
+	});
+
+});
+
+function isValid() {
+	var name = $("#name").val().trim();
+	if ("" === name || null == name) {
+		parent.layer.msg('请输入类型名称！', {
+			icon : 0,
+			time : 2000
+		});
+		return false;
+	}
+
+	return true;
+}
+
+function init() {
+	$(".fixtop").fixtop();
+}
